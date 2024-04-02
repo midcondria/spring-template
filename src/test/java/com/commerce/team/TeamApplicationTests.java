038e4f802bd3b0d210887bbd429680f696921331
midcondria <hyukkind@naver.com>
2024-04-02 21:24:59 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/commerce/team/TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
