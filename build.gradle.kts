8c13eb706717814044d54aed405ddceaa91e6104
midcondria <hyukkind@naver.com>
2024-04-11 20:07:50 +0900

    feat: QueryDSL 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 group = "com.commerce"
 version = "0.0.1-SNAPSHOT"
+val queryDslVersion = "5.0.0" // QueryDSL Version Setting
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -29,6 +30,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	// 스프링 시큐리티
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.session:spring-session-jdbc")
+
 	// 아임포트
 	implementation("com.github.iamport:iamport-rest-client-java:0.2.23")
 
@@ -39,9 +45,16 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.1")
-
 	implementation("org.springframework.security:spring-security-crypto")
 	implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+
+	// QueryDSL
+	implementation("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
+	implementation ("com.querydsl:querydsl-core")
+	implementation ("com.querydsl:querydsl-collections")
+	annotationProcessor("com.querydsl:querydsl-apt:${queryDslVersion}:jakarta")
+	annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+	annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 
 	annotationProcessor("org.projectlombok:lombok")
 	compileOnly("org.projectlombok:lombok")
@@ -52,4 +65,21 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+val querydslDir = layout.buildDirectory.dir("generated/querydsl").get().asFile
+
+sourceSets.getByName("main") {
+	java.srcDir(querydslDir)
+}
+
+tasks.withType<JavaCompile> {
+	options.generatedSourceOutputDirectory.set(file(querydslDir))
+}
+
+// clean 이후에 querydsl 폴더를 지움
+tasks.named("clean") {
+	doLast {
+		file(querydslDir).delete()
+	}
 }
