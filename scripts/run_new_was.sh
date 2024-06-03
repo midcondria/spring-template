ef619ee16e2f205a1f20c89ad983e9ed7fb1e2bb
midcondria <hyukkind@naver.com>
2024-06-03 23:20:50 +0900

    feat: CI/CD 용 스크립트 작성

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,35 @@
+# run_new_was.sh
+
+#!/bin/bash
+
+# 환경 변수 설정
+PROJECT_ROOT="/home/ubuntu/app" # 프로젝트 루트
+JAR_FILE="$PROJECT_ROOT/build/libs/pawland-0.0.1-SNAPSHOT.jar" # 빌드해서 생성된 jar 파일명
+
+# service_url.inc 에서 현재 서비스 중인 WAS의 포트 번호 확인
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082 # 현재 포트가 8081이면 8082로 배포
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081 # 현재 포트가 8082라면 8081로 배포
+else
+  echo "> Not connected to nginx" # nginx가 실행되고 있지 않다면 에러 코드
+fi
+
+# 타겟 포트 번호로 실행 중인 프로세스가 있는지 확인
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+# PID를 이용해 타겟 포트로 실행 중인 프로세스 Kill
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+# 타켓 포트에 jar파일을 이용해 새로운 서버 실행
+nohup java -jar -Dserver.port=${TARGET_PORT} ${JAR_FILE} > /home/ubuntu/nohup.out 2>&1 &
+echo "> Now new WAS runs at ${TARGET_PORT}."
+exit 0
---
+++ scripts/switch.sh
@@ -0,0 +1,34 @@
+# switch.sh
+
+#!/bin/bash
+
+# service_url.inc에서 현재 서비스 중인 WAS의 포트 번호 확인
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    TARGET_PORT=8081
+else
+    echo "> No WAS is connected to nginx"
+    exit 1
+fi
+
+# $ service_url.inc 파일에 적힌 서비스 주소를 새로 띄운 서버의 주소로 변경
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+
+echo "> Now Nginx proxies to ${TARGET_PORT}."
+
+# nginx를 reload 해준다.
+sudo service nginx reload
+
+echo "> Nginx reloaded."
+
+# -9 SIGKILL 은 서버를 바로 종료하므로
+# -15 SIGTERM  안전 종료인 SIGTERM을 사용하여 이전 포트 프로세스를 제거한다.
+CURRENT_PID=$(lsof -Fp -i TCP:${CURRENT_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+sudo kill -15 ${CURRENT_PID}
