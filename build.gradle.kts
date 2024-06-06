c6e2908bba50b0d0aea68a796eec1d400a20407c
midcondria <hyukkind@naver.com>
2024-06-06 21:19:56 +0900

    chore: 스프링 액츄에이터 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	// actuator
+	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+
 	// swagger
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
