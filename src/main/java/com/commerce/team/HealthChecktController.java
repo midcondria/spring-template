97422e1374cb535960baa61b187accb6d035be78
midcondria <hyukkind@naver.com>
2024-06-10 16:05:50 +0900

    feat: 포트번호 확인 로직 추가

--- src/main/java/com/commerce/team/HealthChecktController.java
+++ src/main/java/com/commerce/team/HealthChecktController.java
@@ -1,16 +1,25 @@
 package com.commerce.team;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Slf4j
-public class HealthChecktController {
+@RestController
+public class HealthChecktController implements ApplicationListener<WebServerInitializedEvent> {
+
+    private int serverPort;
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        this.serverPort = event.getWebServer().getPort();
+    }
 
     @GetMapping
     public String health() {
-        log.info("[헬스체크] \"/\" 호출");
-        return "hi";
+        log.info("[헬스체크] 포트번호: {} 호출", serverPort);
+        return "hi" + serverPort;
     }
 }
